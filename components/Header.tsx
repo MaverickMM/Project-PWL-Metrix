d680a2e2c7e9053ee88a2da50bf07f9058b093bc
MaverickMM <78334716+MaverickMM@users.noreply.github.com>
2023-10-21 17:18:48 +0700

    Commit 2

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import classNames from 'classnames';
 import styles from './Header.module.css';
 
 const Header = () => {
+    const router = useRouter();
+
     return (
         <header className={styles.header}>
             <div>
-                <img src="../Pictures/logo.png" alt="Logo" className={styles.logo} />
                 <nav className={styles.nav}>
-                    <Link href="/">Home</Link>
-                    <Link href="/topsongs">Top Songs</Link>
-                    <Link href="/discover">Discover</Link>
-                    <Link href="/detectsong">Detect Song</Link>
+                    <img src="/images/logo.png" alt="Logo" className={styles.logo} />
+                    <Link href="/">
+                        <span className={classNames(styles.link, { [styles.active]: router.pathname === '/' })}>Home</span>
+                    </Link>
+                    <Link href="/topsongs">
+                        <span className={classNames(styles.link, { [styles.active]: router.pathname === '/topsongs' })}>Top Songs</span>
+                    </Link>
+                    <Link href="/discover">
+                        <span className={classNames(styles.link, { [styles.active]: router.pathname === '/discover' })}>Discover</span>
+                    </Link>
+                    <Link href="/detectsong">
+                        <span className={classNames(styles.link, { [styles.active]: router.pathname === '/detectsong' })}>Detect Song</span>
+                    </Link>                    
                 </nav>
             </div>
             <input type="text" placeholder="Search..." className={styles.searchBar} />
