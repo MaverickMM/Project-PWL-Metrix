e7573abfc320979170a1e72adb50a955d8f68cbb
EdithFelicia <78738551+EdithFelicia@users.noreply.github.com>
2023-10-23 05:14:05 +0700

    Add files via upload

--- pages/discover.tsx
+++ pages/discover.tsx
@@ -1,214 +1,91 @@
-import React, { useState } from 'react';
-import '../public/styles/global.css';
+import React from 'react';
 import Header from '../components/Header';
-import styles from './Discover.module.css';
+import Footer from '../components/Footer';
+import dynamic from 'next/dynamic';
+import '../public/styles/global.css';
+import '../public/styles/card1.css';
 import Dropdown from './Dropdown';
 
-const MusicDiscoveryPage: React.FC = () => {
-   const [selectedRegion, setSelectedRegion] = useState<string>('');
+// Dynamically import Card with SSR disabled
+const Card = dynamic(() => import('../components/CardDiscover'), { ssr: false });
 
-   const handleRegionSelect = (region: string) => {
-     setSelectedRegion(region);
-   };
+const Discover = () => {
+  const cardData = [
+    {
+        song_title: 'As the World Caves In',
+        song_description: 'Sarah Cothran',
+        imageUrl: '/Images/As The World Caves In.jpg',
+    },
+    {
+        song_title: '(It Goes Like) Nanana',
+        song_description: 'Peggy Gou',
+        imageUrl: '/Images/(It Goes Like) Nanana.jpg',
+    },
+    {
+        song_title: 'Jiwa Yang Bersedih',
+        song_description: 'Ghea Indrawari',
+        imageUrl: '/Images/Jiwa yang Bersedih.jpg',
+    },
+    {
+        song_title: 'Tattoo',
+        song_description: 'Loreen',
+        imageUrl: '/Images/Tatto.png',
+    },
+    {
+        song_title: 'Satu-Satu',
+        song_description: 'Idgitaf',
+        imageUrl: '/Images/Satu-Satu.jpg',
+    },
+    {
+        song_title: 'Moonlight',
+        song_description: 'Kali Uchis',
+        imageUrl: '/Images/Moonlight.png',
+    },
+    {
+        song_title: 'Somebodys Pleasure',
+        song_description: 'Aziz Hedra',
+        imageUrl: '/Images/Somebodys Pleasure.jpg',
+    },
+    {
+        song_title: 'Rayuan Perempuan Gila',
+        song_description: 'Nadin Amizah',
+        imageUrl: '/Images/Rayuan Perempuan Gila.jpg',
+    },
+    {
+        song_title: 'Popular (feat. Playboi Carti)',
+        song_description: 'The Weeknd & Madonna',
+        imageUrl: '/Images/Popular.jpg',
+    },
+    {
+        song_title: ' Paint The Town Red',
+        song_description: 'Doja Cat',
+        imageUrl: '/Images/Paint the Town Red.jpg',
+    },
+];
 
-   return (
-     <div>
-        <Header />
-        <Dropdown onSelect={handleRegionSelect} />
-        {selectedRegion && (
-        <p>Selected Region: {selectedRegion}</p>)}
-        
-        <div className={styles.discover1}>
-        <div className={styles.content}>
-            <div className={styles.top}>
-            <div className={styles.theMostTrending}>
-                The most trending tracks in Indonesia this week
-            </div>
-            </div>
-        </div>
-        </div>
-        <div className={styles.container}>
-            <div className={styles.thumbnail}>
-                <div className={styles.containerCard}>
-                <img 
-                    className={styles.imageThumbIcon}
-                    alt=""
-                    src="/Images/As The World Caves In.jpg"
-                />
-                </div>
-                <div className={styles.contentCard}>
-                <a
-                    className={styles.titleThumb}
-                    href="https://music.apple.com/id/album/as-the-world-caves-in/1578612434?i=1578612440"
-                    target="_blank"> As the World Caves In
-                </a>
-                <div className={styles.authorThumb}>
-                    Sarah Cothran</div>
-                <div className={styles.labelThumb} />
-                </div>
-            </div>
-            <div className={styles.thumbnail}>
-                    <div className={styles.containerCard}>
-                    <img
-                        className={styles.imageThumbIcon}
-                        alt=""
-                        src="/Images/(It Goes Like) Nanana.jpg"
-                    />
-                    </div>
-                    <div className={styles.contentCard}>
-                    <a
-                        className={styles.titleThumb}
-                        href="https://music.apple.com/id/album/it-goes-like-nanana-edit/1689088940?i=1689088958"
-                        target="_blank">
-                        (It Goes Like) Nanana (Edit)
-                    </a>
-                    <div className={styles.authorThumb}>
-                        Peggy Gou</div>
-                    <div className={styles.labelThumb} />
-                    </div>
-                </div>
-                <div className={styles.thumbnail}>
-                    <div className={styles.containerCard}>
-                    <img
-                        className={styles.imageThumbIcon}
-                        alt=""
-                        src="/Images/Jiwa yang Bersedih.jpg"
-                    />
-                    </div>
-                    <div className={styles.contentCard}>
-                    <div className={styles.titleThumb}>
-                        Jiwa Yang Bersedih</div>
-                    <div className={styles.authorThumb}>
-                        Ghea Indrawari</div>
-                    <div className={styles.labelThumb} />
-                    </div>
-                </div>
-                <div className={styles.thumbnail}>
-                    <div className={styles.containerCard}>
-                    <img
-                        className={styles.imageThumbIcon}
-                        alt=""
-                        src="/Images/Tatto.png"
-                    />
-                    </div>
-                    <div className={styles.contentCard}>
-                    <div className={styles.titleThumb}>
-                        Tattoo</div>
-                    <div className={styles.authorThumb}>
-                        Loreen</div>
-                    <div className={styles.labelThumb} />
-                    </div>
-                </div>
-                <div className={styles.thumbnail}>
-                    <div className={styles.containerCard}>
-                    <img
-                        className={styles.imageThumbIcon}
-                        alt=""
-                        src="/Images/Satu-Satu.jpg"
-                    />
-                    </div>
-                    <div className={styles.contentCard}>
-                    <div className={styles.titleThumb}>
-                        Satu-Satu</div>
-                    <div className={styles.authorThumb}>
-                        Idgitaf</div>
-                    <div className={styles.labelThumb} />
-                    </div>
-                </div>
-                <div className={styles.thumbnail}>
-                    <div className={styles.containerCard}>
-                    <img
-                        className={styles.imageThumbIcon}
-                        alt=""
-                        src="/Images/Moonlight.png"
-                    />
-                    </div>
-                    <div className={styles.contentCard}>
-                    <div className={styles.titleThumb}>
-                        Moonlight</div>
-                    <div className={styles.authorThumb}>
-                        Kali Uchis</div>
-                    <div className={styles.labelThumb} />
-                    </div>
-                </div>
-                <div className={styles.thumbnail}>
-                    <div className={styles.containerCard}>
-                    <img
-                        className={styles.imageThumbIcon}
-                        alt=""
-                        src="/Images/Somebody's Pleasure.jpg"
-                    />
-                    </div>
-                    <div className={styles.contentCard}>
-                    <div className={styles.titleThumb}>
-                        Somebody's Pleasure</div>
-                    <div className={styles.authorThumb}>
-                        Aziz Hedra</div>
-                    <div className={styles.labelThumb} />
-                    </div>
-                </div>
-                <div className={styles.thumbnail}>
-                    <div className={styles.containerCard}>
-                    <img
-                        className={styles.imageThumbIcon}
-                        alt=""
-                        src="/Images/Rayuan Perempuan Gila.jpg"
-                    />
-                    </div>
-                    <div className={styles.contentCard}>
-                    <div className={styles.titleThumb}>
-                        Rayuan Perempuan Gila</div>
-                    <div className={styles.authorThumb}>
-                        Nadin Amizah</div>
-                    <div className={styles.labelThumb} />
-                    </div>
-                </div>
-                <div className={styles.thumbnail}>
-                    <div className={styles.containerCard}>
-                    <img
-                        className={styles.imageThumbIcon}
-                        alt=""
-                        src="Images/Popular.jpg"
-                    />
-                    </div>
-                    <div className={styles.contentCard}>
-                    <div className={styles.titleThumb}>
-                        Popular (feat. Playboi Carti) </div>
-                    <div className={styles.authorThumb}> 
-                        The Weeknd & Madonna </div>
-                    <div className={styles.labelThumb} />
-                    </div>
-                </div>
-                <div className={styles.thumbnail}>
-                    <div className={styles.containerCard}>
-                    <img
-                        className={styles.imageThumbIcon}
-                        alt=""
-                        src="/Images/Paint the Town Red.jpg"
-                    />
-                    </div>
-                    <div className={styles.contentCard}>
-                    <div className={styles.titleThumb}>
-                        Paint The Town Red </div>
-                    <div className={styles.authorThumb}>
-                        Doja Cat </div>
-                    <div className={styles.labelThumb}/>
-                    </div>
-                </div>
+  return (
+        <div>
+            <Header />
+            <main>
+                <h3 className='title'>The most trending tracks in Indonesia this week</h3>
 
-        </div>
-        <div className={styles.footer}>
-        <div className={styles.copyrightContainer}>
-            <p className={styles.copyright}>Copyright 2023 Metrix</p>
-            <p className={styles.copyright}>&nbsp;</p>
-            <p className={styles.copyright}>Maverick Mikolas Mulyadi</p>
-            <p className={styles.copyright}>Edith Felicia Putri</p>
-            <p className={styles.copyright}>Beatrix Stefany</p>
-        </div>
-        </div>
+                <br/>
+                <br/>
+               
+                {/* Pass the cardData object as props */}
+                <div className="containerAll">
+                {cardData.map((card, index) => (
+                    <Card
+                        key={index}
+                        song_title={card.song_title} 
+                        song_description={card.song_description} 
+                        imageUrl={card.imageUrl} />
+                    ))}
+                </div>
+            </main>
+            <Footer />
         </div>
     );
 };
-export default MusicDiscoveryPage;
 
-
+export default Discover;
