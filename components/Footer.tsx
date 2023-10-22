36bbd4caa377da164b252b179483afa2f38103a9
EdithFelicia <78738551+EdithFelicia@users.noreply.github.com>
2023-10-23 05:15:13 +0700

    Add files via upload

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.copyrightContainer}>
                 <p className={styles.copyright}>Copyright 2023 Metrix</p>
-                <p className={styles.copyright}>&nbsp;</p>
+                <br/>
                 <p className={styles.copyright}>Maverick Mikolas Mulyadi</p>
                 <p className={styles.copyright}>Edith Felicia Putri</p>
                 <p className={styles.copyright}>Beatrix Stefany</p>
@@ -19,3 +19,6 @@ const Footer = () => {
 };
 
 export default Footer;
+
+
+    
