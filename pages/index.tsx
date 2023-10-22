4a0f457f61ba7b45532f9eeda5e89fc88bf06122
beatrixstefany <beatrix.stefany@ti.ukdw.ac.id>
2023-10-22 13:00:34 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Card from '../components/Card';
 import '../styles/global.css';
-import '../styles/card.css';
+// import '../styles/card.css';
 
 const HomePage = () => {
     const cards = [
